14259f28c5f7f4af9288234fb88dd43b01e8dddd
Rafael <oraffaudi@gmail.com>
2019-07-20 19:47:55 -0300

    Fisrt migration with sequelize cli / create Model User

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,16 @@
 import { Router } from 'express';
 
+import User from './app/models/User';
+
 const routes = new Router();
 
-routes.get('/', (req, res) => {
-  return res.json({ 'Teste teste': 'Teste' });
+routes.get('/', async (req, res) => {
+    const user = await User.create({
+        name: 'Rafael Henrique de Freitas',
+        email: 'oraffaudi@gmail.com',
+        password_hash: '1234456879897 '
+    });
+    return res.json(user);
 });
 
 export default routes;
