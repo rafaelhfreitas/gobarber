3c272a1f5473e7ce35125b728c8914d4e15e25d9
Rafael <oraffaudi@gmail.com>
2019-07-20 20:40:27 -0300

    Feature create user

--- src/routes.js
+++ src/routes.js
@@ -1,16 +1,9 @@
 import { Router } from 'express';
 
-import User from './app/models/User';
+import UserController from './app/controllers/UserController';
 
 const routes = new Router();
 
-routes.get('/', async (req, res) => {
-    const user = await User.create({
-        name: 'Rafael Henrique de Freitas',
-        email: 'oraffaudi@gmail.com',
-        password_hash: '1234456879897 '
-    });
-    return res.json(user);
-});
+routes.post('/users', UserController.store);
 
 export default routes;
